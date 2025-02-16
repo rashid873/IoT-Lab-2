5859bf4727117f5c4e4e98faf383ae7ba0d3e3af
rashid873 <rashidlateef873@gmail.com>
2025-02-16 21:12:42 +0500

    Updated main.py with new functionality

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # main.py -- put your code here!
+from machine import Pin
+from neopixel import NeoPixel
+import time
+
+pin = Pin(48, Pin.OUT)   
+neo = NeoPixel(pin, 1)   
+
+while True:
+    neo[0] = (0, 255, 0)  
+    neo.write()           
+    time.sleep(1)
+    
+    neo[0] = (255, 0, 0)  
+    neo.write()           
+    time.sleep(2)      
+    
+    neo[0] = (0, 0, 255) 
+    neo.write()           
+    time.sleep(2)
